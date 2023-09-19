ab90512b85b2ac046378c02f1784477b29e43635
Mr. Erlison <sisal.cordame_0a@icloud.com>
2023-09-19 17:55:30 -0300

    Update to use the sort lib

--- exercises/anagram/anagram.go
+++ exercises/anagram/anagram.go
@@ -2,16 +2,22 @@ package anagram
 
 import (
 	"reflect"
-	"slices"
+	"sort"
 	"strings"
 )
+
+type letters []string
+
+func (s letters) Len() int           { return len(s) }
+func (s letters) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s letters) Less(i, j int) bool { return s[i] < s[j] }
 
 func Detect(subject string, candidates []string) []string {
 	var anagrams []string = []string{}
 
 	rearrangement := func(word string) []string {
-		w := strings.Split(word, "")
-		slices.Sort(w)
+		w := letters(strings.Split(word, ""))
+		sort.Sort(w)
 		return w
 	}
 
